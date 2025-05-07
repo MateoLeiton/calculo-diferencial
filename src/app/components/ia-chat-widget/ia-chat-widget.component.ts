ffbc0f94a07ef6f89fbdd30e4fbadce4a726a1c7
MateoLeiton <mateoleytoncal@gmail.com>
2025-05-07 16:52:04 -0500

    Revertir el commit 56fe0f8

--- src/app/components/ia-chat-widget/ia-chat-widget.component.ts
+++ src/app/components/ia-chat-widget/ia-chat-widget.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, HostListener } from '@angular/core';
+// src/app/components/ia-chat-widget/ia-chat-widget.component.ts
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-ia-chat-widget',
   standalone: true,
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule],
+  selector: 'app-ia-chat-widget',
   templateUrl: './ia-chat-widget.component.html',
   styleUrls: ['./ia-chat-widget.component.scss']
 })
@@ -13,49 +13,10 @@ export class IaChatWidgetComponent {
   @Input() title: string = 'IA';
   @Input() color: string = '#2196f3';
   @Input() minimizedLabel: string = 'IA';
-  
-  isOpen = false;
-  width = 300;
-  height = 400;
 
-  zIndex = 1;
-  static currentMaxZ = 1;
+  isOpen = false;
 
   toggleChat() {
     this.isOpen = !this.isOpen;
-    if (this.isOpen) {
-      this.bringToFront();
-    }
   }
-
-  @HostListener('mousedown')
-  bringToFront() {
-    IaChatWidgetComponent.currentMaxZ++;
-    this.zIndex = IaChatWidgetComponent.currentMaxZ;
-  }
-
-  initResize(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-  
-    const startX = event.clientX;
-    const startY = event.clientY;
-    const startWidth = this.width;
-    const startHeight = this.height;
-  
-    const mouseMoveHandler = (moveEvent: MouseEvent) => {
-      this.width = startWidth + (moveEvent.clientX - startX);
-      this.height = startHeight + (moveEvent.clientY - startY);
-    };
-  
-    const mouseUpHandler = () => {
-      document.removeEventListener('mousemove', mouseMoveHandler);
-      document.removeEventListener('mouseup', mouseUpHandler);
-    };
-  
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-  }
-  
-
 }
